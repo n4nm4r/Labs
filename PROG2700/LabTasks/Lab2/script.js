b989005f7e39e510a63d1be51c45dd7be5a52f9b
n4nm4r <144350280+n4nm4r@users.noreply.github.com>
2024-01-19 13:56:27 +0000

    Friday commit

---
+++ PROG2700/LabTasks/Lab2/script.js
@@ -0,0 +1,20 @@
+// Access the div with the id 'output'
+const outputDiv = document.getElementById('output');
+
+// Log a message to the console
+console.log('Hello from the console!');
+
+// Display the same message on the HTML page
+outputDiv.innerText = 'Hello from the HTML!';
+
+
+var city = "Halifax";
+
+if (city = "Halifax") {
+    console.log("Current city:Halifax.");
+}else if (city != "Halifax") {
+    console.log("What is your current city?")
+  }else {
+    console.log("Please choose a city");
+}
+
